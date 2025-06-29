8f551ad1b279e8b8dfc7c9f03f1c7db120be3277
KezelHugo <kevin.bsl.2001@hotmail.com>
2025-06-28 19:07:26 -0500

    Proyecto separado: ms-orders

---
+++ src/test/java/com/msorders/MsordersApplicationTests.java
@@ -0,0 +1,14 @@
+package com.msorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Este test asegura que el contexto de Spring Boot se cargue correctamente.
+	}
+
+}
